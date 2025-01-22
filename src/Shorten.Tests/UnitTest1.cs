8406e3d36009eadd0460945b0f9907e333192ae4
Salinas-Condori-Erick <es2020069046@virtual.upt.pe>
2025-01-22 02:06:01 -0500

    despliegue_Aplicacion_9

---
+++ src/Shorten.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Shorten.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Ejemplo: Prueba básica para validar que 2 + 2 es igual a 4
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
